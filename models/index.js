fc92375f9413dcd0817742a29248c44451c148ff
Tomi Hakkarainen <tpaivaa@gmail.com>
2022-12-26 19:00:05 +0200

    no naäin

--- models/index.js
+++ models/index.js
@@ -1,42 +1,47 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const fs = require('fs')
+const path = require('path')
+const path = require('path')
+const Sequelize = require('sequelize')
+const basename = path.basename(__filename)
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.json')[env]
+const db = {}
 
 console.log('Environment: ', env)
 console.log(basename)
-let sequelize;
+let sequelize
 if (config.use_env_variable) {
   console.log(process.env[config.use_env_variable])
   console.log(config)
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {logging: (...msg) => console.log(msg)},config);
+  sequelize = new Sequelize(config.database, config.username, config.password,
+     {logging: (...msg) => console.log(msg),
+      storage: path.join(__dirname, '..', 'local.db'),
+      dialect: 'sqlite'
+    });
   console.log("Else: ", config)
 }
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    db[model.name] = model
+  })
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db);
+    db[modelName].associate(db)
   }
-});
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
